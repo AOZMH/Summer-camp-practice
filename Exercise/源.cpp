5655444193206c42063819d7ca8045805998dd4a
AOZMH <zhangminhao@pku.edu.cn>
2020-06-27 20:34:02 +0800

    1-7

--- Exercise/源.cpp
+++ Exercise/源.cpp
@@ -18,41 +18,7 @@
 
 int main()
 {   
-    //SOLVE_3();
-
-    int N, K, lens[10005];
-    cin >> N >> K;
-    float tmp;
-    for (int i = 0; i < N; ++i) {
-        cin >> tmp;
-        lens[i] = tmp * 100;
-    }
-    int l_len = 0, r_len = 10000002;
-
-    while ((l_len < r_len - 1)) {
-        int mid = (l_len + r_len) / 2;
-        if (mid == 0)
-            break;
-        int avail = 0;
-        for (int i = 0; i < N; ++i) {
-            avail += lens[i] / mid;
-            if (avail >= K)
-                break;
-        }
-        if (avail >= K) {
-            l_len = mid;
-            continue;
-        }
-        else {
-            r_len = mid;
-            continue;
-        }
-    }
-
-    //printf("%.2f", float(l_len) / 100);
-    cout.flags(ios::fixed);
-    cout.precision(2);
-    cout << float(l_len) / 100;
+    SOLVE_3();
 
     return 0;
 }
